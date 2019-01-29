4acf034e8ce30780519875640270a4b3c16b749f
eagletommy52 <andrew.m.thompson@outlook.com>
2019-01-29 10:31:30 -0500

    added query-string dep

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,8 +1,19 @@
 const React = require('react');
-
+const queryString = require('query-string');
+const api = require('../utils/api')
 class Results extends React.Component {
+  componentDidMount() {
+    let players = queryString.parse(this.props.location.search)
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName
+    ])
+    .then(results=>{
+      console.log(results)
+    })
+  }
   render(){
-    console.log(this.props)
+    
     return (
       <div>Results</div>
     )
