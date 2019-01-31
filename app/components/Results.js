47ef63466e68183f1d5251e80fb17f37dd38cd19
eagletommy52 <andrew.m.thompson@outlook.com>
2019-01-31 10:27:06 -0500

    added es-lint config

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,23 +1,23 @@
 const React = require('react');
 const queryString = require('query-string');
-const api = require('../utils/api')
+const api = require('../utils/api');
 class Results extends React.Component {
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true,
+  };
   componentDidMount() {
-    let players = queryString.parse(this.props.location.search)
-    api.battle([
-      players.playerOneName,
-      players.playerTwoName
-    ])
-    .then(results=>{
-      console.log(results)
-    })
+    let players = queryString.parse(this.props.location.search);
+    api.battle([players.playerOneName, players.playerTwoName])
+      .then(results => {
+      console.log(results);
+    });
   }
-  render(){
-    
-    return (
-      <div>Results</div>
-    )
+  render() {
+    return <div>Results</div>;
   }
 }
 
-module.exports = Results
+module.exports = Results;
